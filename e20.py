cd7dcfb9b3945f0369fc60f067ed42b9eb95ac60
Jesus Rodriguez Falcón <alu0100099010@ull.edu.es>
2014-04-02 08:51:04 +0100

    Update e20.py

--- e20.py
+++ e20.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python
 #!encoding: UTF-8
 
-/*Dados los resultados de una prueba de nivel realizada a un alumno se conoce la cantidad total de
-preguntas realizadas y la cantidad de respuestas correctas. Escribir un programa que informar
-del nivel registrado de acuerdo a la siguiente escala: a) Muy Bueno si el porcentaje es mayor o
-igual a 90% b) Bueno entre 70% y 90% c) Regular entre 50% y 70% d) Malo si el porcentaje
-es menor que 50%.*/
+
 
 x = -b/a  # la asignación debe estar despues de b=float....
 a = float(raw_input(’Valor de a: ’))
